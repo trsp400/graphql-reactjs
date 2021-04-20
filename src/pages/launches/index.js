99baa5f01e9a31960af6031cba8170d0faaf14db
Thiago Robles <roblesthiago38@gmail.com>
2021-04-19 22:18:28 -0300

    chore: style launches screen

--- src/pages/launches/index.js
+++ src/pages/launches/index.js
@@ -1,18 +1,28 @@
 import Card from "../../components/Card";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Heading, Input, Flex, Center } from "@chakra-ui/react";
 
 const Launches = () => {
   return (
-    <Grid>
-      <GridItem
-        padding="10"
-        width="20%"
-        templateRows="repeat(10, 5fr)"
-        templateColumns="repeat(5, 1fr)"
-      >
-        <Card />
-      </GridItem>
-    </Grid>
+    <Flex flexDirection="column" flex="1" w="100%">
+      <Flex padding="10" flexDirection="row" justifyContent="space-between">
+        <Heading fontSize="3xl">Launches</Heading>
+        <Flex justifyContent="space-around" w="50%">
+          <Center>
+            <Input type="text" placeholder="Pesquisar" w="64" />
+            <Heading paddingLeft="10" fontSize="medium">
+              Apresentando: 10
+            </Heading>
+          </Center>
+        </Flex>
+      </Flex>
+      <Flex>
+        <Grid padding="10" w="80%">
+          <GridItem>
+            <Card />
+          </GridItem>
+        </Grid>
+      </Flex>
+    </Flex>
   );
 };
 
