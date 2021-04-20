bb6d84b698ea7d362610ac5f5fb6e10240c10fba
Thiago Robles <roblesthiago38@gmail.com>
2021-04-19 22:49:47 -0300

    chore: remove unecessary code on App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
-import { useQuery, gql } from "@apollo/client";
-
 import Routes from "./routes";
 
-const launchesPast = gql`
-  query GetExchangeRates {
-    launchesPast(limit: 10) {
-      mission_name
-      launch_date_local
-      launch_site {
-        site_name_long
-      }
-    }
-  }
-`;
-
 const App = () => {
-  const { loading, error, data } = useQuery(launchesPast);
-
   return <Routes />;
 };
 
